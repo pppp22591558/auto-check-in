d903fe1152d180c5abf53d6a7bb898e3e29d31b8
NeilZheng <pppp29654213@gmail.com>
2016-12-05 18:57:13 +0800

    index

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  check_in_hour: 18,
+  check_out_hour: 10,
+  username: 'abc',
+  password: '1234567'
+}
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const schedule = require('node-schedule');
 const Nightmare = require('nightmare');
+const config = require('./config');
 
 const nightmare = Nightmare();
 
 const checkInRule = new schedule.RecurrenceRule();
 checkInRule.dayOfWeek = [0, new schedule.Range(1, 5)];
-checkInRule.hour = 1;
+checkInRule.hour = config.check_in_hour;
 checkInRule.minute = 0;
 
 const checkOutRule = new schedule.RecurrenceRule();
 checkOutRule.dayOfWeek = [0, new schedule.Range(1, 5)];
-checkOutRule.hour = 10;
+checkOutRule.hour = config.check_out_hour;
 checkOutRule.minute = 0;
 
 const i = schedule.scheduleJob(checkInRule, function() {
@@ -33,8 +34,8 @@ function startCheck(option) {
   return nightmare
     .goto('https://femascloud.com/bonio2016/accounts/login')
     .wait('#user_username')
-    .type('input#user_username', _____) //帳號
-    .type('input#user_passwd', _____) //密碼
+    .type('input#user_username', config.username) //帳號
+    .type('input#user_passwd', config.password) //密碼
     .click('#s_buttom')
     .wait('.clock_enabled')
     .evaluate(function(){
